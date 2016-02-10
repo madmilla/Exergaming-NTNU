e033a6b75dc674afd4e40a0f94103e9bfae396eb
Lars Veenendaal <madmilla@gmail.com>
2016-02-09 17:32:01 -0800

    First WPF Build.

---
+++ KinectTesting/KinectTesting/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+
+namespace KinectTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+        KinectSensor sensor;
+        DepthFrameReader reader;
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.sensor = KinectSensor.GetDefault();
+            this.sensor.Open();
+            this.reader = this.sensor.DepthFrameSource.OpenReader();
+            this.reader.FrameArrived += OnFrameArrived;
+        }
+
+        void OnFrameArrived(object sender, DepthFrameArrivedEventArgs e)
+        {
+            using(var frame = e.FrameReference.AcquireFrame())
+            {
+                if(frame != null)
+                {
+                    camera.Source = ProcessFrame(frame);
+                }
+            }
+        }
+
+        private ImageSource ProcessFrame(DepthFrame frame)
+        {
+            int width = frame.FrameDescription.Width;
+            int height = frame.FrameDescription.Height;
+            PixelFormat format = PixelFormats.Bgr32;
+            ushort minDepth = frame.DepthMinReliableDistance;
+            ushort maxDepth = frame.DepthMaxReliableDistance;
+            ushort[] pixelData = new ushort[width * height];
+            byte[] pixels = new byte[width * height * (format.BitsPerPixel + 7) / 8];
+            frame.CopyFrameDataToArray(pixelData);
+            int colorIndex = 0;
+            for (int depthIndex = 0; depthIndex < pixelData.Length; ++depthIndex)
+            {
+                ushort depth = pixelData[depthIndex];
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+                pixels[colorIndex++] = intensity; // Blue
+                pixels[colorIndex++] = intensity; // Green
+                pixels[colorIndex++] = intensity; // Red
+                ++colorIndex;
+            }
+            int stride = width * format.BitsPerPixel / 8;
+            return BitmapSource.Create(width, height, 96, 96, format, null, pixels, stride);
+        }
+    }
+}
